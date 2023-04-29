e084d97fdfee85de35ddbce9bd86180bbb797e9e
reshunevi <reshmin05paps@gmail.com>
2023-04-28 19:01:37 -0800

    task 4

---
+++ day4/task4a.js
@@ -0,0 +1,5 @@
+let obj1 = {name : "Person 1", age : 5};
+let obj2 = {age : 5, name : "Person 1"};
+        if(Object.keys(obj1).length == Object.keys(obj2).length){
+            console.log("The Values are equal");
+        }
