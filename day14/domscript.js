aeee1debcc0506e12a98c948e38fc264ed23963b
reshunevi <reshmin05paps@gmail.com>
2023-05-19 06:19:08 -0800

    first commit

---
+++ day14/domscript.js
@@ -0,0 +1,17 @@
+// define data
+
+let arr = [
+    {name: 'player1', score: 10}, {name: 'player1', score: 10}, {name: 'player1', score: 10}
+    {name: 'player1', score: 10}
+   
+];//select the container div
+let container = document.getElementById('container');
+
+let table = document.getElementById('table');
+
+
+
+//populate the header and data of the table
+
+
+container.appendChild(table);
