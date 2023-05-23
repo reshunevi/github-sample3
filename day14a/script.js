c81c0f4f8b2dc5b55bf02d55a9e732dda95a860a
reshunevi <reshmin05paps@gmail.com>
2023-05-22 22:09:10 -0800

    first commit

---
+++ day14a/script.js
@@ -0,0 +1,61 @@
+// console.log(window.location);
+//console.log(window.location.href); // current page url
+  
+
+//console.log(window.location.hostname);
+// console.log(window.location.protocol);
+
+// console.log(window.location.search); // query parameters
+
+// window.localStorage.setItem('username', 'Sathish')
+// console.log(window.localStorage);
+// console.log(window.localStorage.getItem('username'));
+// console.log(window.localStorage.removeItem('username'));
+// window.localStorage.setItem('isLoggedIn', true)
+// set Timeout
+// let sayHello = () =>{
+//     console.log('after five seconds');
+// }
+// window.setTimeout(sayHello, 2000);
+
+// let sayHello = () =>{
+//     console.log('after every two seconds');
+// }
+// window.setInterval(sayHello, 2000);
+
+// let sayHello = () =>{
+//     console.log('Hello');
+// }
+// const sayHelloID = window.setInterval(sayHello, 5000);
+// window.clearInterval(sayHelloID);
+
+// function countDownTimer(seconds){
+//     if(seconds>0){
+//         console.log(seconds);
+//        setTimeout(() => {
+//         countDownTimer(seconds - 1);
+//        }, 1000);
+//     }else{
+            
+//         console.log('Happy New Year');
+//     }
+// }
+// countDownTimer(10);
+
+// let seconds = 10;
+// let timer = setInterval(() => {
+//     console.log(seconds);
+//     seconds--;
+// }, 1000);
+
+// setTimeout(() => {
+//     clearInterval(timer);
+//     setTimeout(()=> {
+//         console.log('Happy New Year');
+    
+//     }, 1000);
+// }, 1000);
+
+
+
+    
