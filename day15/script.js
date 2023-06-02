717ffd289dccbee315e0db49e6f41386e99aa5ca
reshunevi <reshmin05paps@gmail.com>
2023-06-02 04:32:22 -0800

    stage  changes

---
+++ day15/script.js
@@ -0,0 +1,28 @@
+//get reference of the span
+// let spanColor = document.getElementById('spanColor');
+// console.log(spanColor);
+// spanColor.addEventListener('mouseover', function(event){
+//     spanColor.style.color = 'red';
+// });
+// spanColor.addEventListener('mouseout', function(event){
+//     spanColor.style.color = 'green';
+   
+// });
+
+//Task2
+
+spanInput.onclick = function(){
+    var name=prompt("Hey! Welcome to Validus! Whats your name?", "Name");
+    document.getElementById("welcomeText").innerHTML = name;
+   }
+
+   //Task3
+
+//    function showCoordinates(event){
+//     let header = document.getElementById('header');
+//     let x=event.clientX;
+//     let y=event.clientY;
+//     let spanCoordinates=
+//    }
+//incomplete
+
