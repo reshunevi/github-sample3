c5a92ea848e1238afe8df09166ab5fd96ddfd384
reshunevi <reshmin05paps@gmail.com>
2023-06-02 04:34:09 -0800

    stage changes

---
+++ day15/Task3-script.js
@@ -0,0 +1,15 @@
+//create a div
+let containerDIV = document.createElement('div');
+
+// add class
+containerDIV.setAttribute('class', 'container');
+containerDIV.classList.add('text-center');
+
+for(let i =1; i<=6; i++){
+    let rowDIV = document.createElement('div');
+    
+}
+//add it to body element
+document.body.appendChild(containerDIV);
+
+
