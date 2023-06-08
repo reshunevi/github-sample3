955fd5ea53bf85e00871e23f81d7d4f7c829660f
reshunevi <reshmin05paps@gmail.com>
2023-06-08 21:51:15 +0530

    function

---
+++ day22-react-day1/functions.js
@@ -0,0 +1,51 @@
+// function sayHello(){
+//     console.log('hello');
+
+// } sayHello();
+// sayHello();
+
+// var sayHello = function () {
+//     console.log('hello');
+// } 
+// sayHello ();
+
+// var sayHello = () => {
+//     console.log('hi');
+// } 
+// sayHello ();
+
+// var sayHello = () => console.log('hi hello');
+// sayHello ();
+
+//power function
+
+// let power = (x, y) => {
+//     return x*y;
+
+// }
+// console.log(power(3, 3));
+
+// let power = (x, y) => x**y;
+
+
+// console.log(power(5, 2));
+
+//IIFE
+//Anonymous Function- without function name
+// let add = function (x, y) {
+//     return x+y ;
+// };
+
+// let sub = (x, y) => {
+//     return x - y;
+// };
+
+//IIFE- immediatelyy invoked function
+// (function (x, y) {
+//     console.log(x+y);
+// })(3, 2);
+
+// console.log((function (x, y) {
+//     return (x+y);
+// })(3, 2));
+
