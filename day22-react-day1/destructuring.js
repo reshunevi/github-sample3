9b57155d3dc34ea94744c68fd67702d1b3b981ed
reshunevi <reshmin05paps@gmail.com>
2023-06-08 21:50:42 +0530

    destructure

---
+++ day22-react-day1/destructuring.js
@@ -0,0 +1,47 @@
+// array & oject destructuring
+
+// let numbers = [1000, 2, 5];
+// let prinicipalAmount = numbers[0];
+// let numberOfYears = numbers[1];
+// let rateOfInterest = numbers[2];
+
+//destructuring
+// let [prinicipalAmount, numberOfYears, rateOfInterest] = numbers;
+
+// console.log(prinicipalAmount, numberOfYears, rateOfInterest);
+
+// let numbers = [1, 2, 3];
+// let [a, b, c, d=4] = numbers;
+// console.log(a, b, c, d);
+
+// let a = 5;
+// let b = 6;
+
+// console.log(`before swapping : ${a}, b = ${b}`);
+
+//swapping using third variable
+// let temp = a;
+// a = b;
+// b = a;
+
+
+//destructuring
+// [a, b] = [b, a];
+// console.log(`after swapping : ${a}, b = ${b}`);
+// oject destructuring
+
+// let person ={
+//     name: "Ivan",
+//     age: 20
+// };
+
+// let {name, age} = person;
+
+// let {name: nameOfPerson, age: ageOfPerson} = person;
+
+// console.log(name, age);
+
+// console.log(person.name, person.age);
+
+// console.log(nameOfPerson, ageOfPerson);
+
