4318bb48d7c88e799b6de4a956af07be7b2f50bf
reshunevi <reshmin05paps@gmail.com>
2023-06-08 21:51:04 +0530

    ES5 and ES6

---
+++ day22-react-day1/esvses6.js
@@ -0,0 +1,26 @@
+/* 
+    ES5
+    1. variable declaration
+        -var
+    2. function keyword
+
+    ES6
+    1. variable declaration
+        - Features:
+        - arrow functions
+        - template literals
+        - destructuring
+        -Variales
+            - let
+            - const
+        - Classes
+            -class
+            -constructor
+        - Promises
+        - functions 
+            - default parameter values       
+    2. arrow functions
+
+    3.
+*/
+
