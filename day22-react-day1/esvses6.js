aa9c3b9a91d1c0ac3f8626de06cf93f01cf9d377
reshunevi <reshmin05paps@gmail.com>
2023-06-09 07:52:54 +0530

    updated

--- day22-react-day1/esvses6.js
+++ day22-react-day1/esvses6.js
@@ -1,26 +1,41 @@
 /* 
-    ES5
+    ES5 - 2009
     1. variable declaration
         -var
     2. function keyword
+    3. return
+    4. primitive data types like string number null boolean and undefined
 
-    ES6
+    ES6 - 2015
     1. variable declaration
         - Features:
         - arrow functions
+        - new primitive data type symbol
         - template literals
         - destructuring
-        -Variales
+        -Variales:
             - let
             - const
-        - Classes
+        - Classes:
             -class
             -constructor
         - Promises
-        - functions 
-            - default parameter values       
-    2. arrow functions
-
-    3.
+        - functions:
+            - default parameter values 
+        - operators
+            - rest
+            - spread
+        - loops
+            - for...of loop
+        - datastructures
+            - Map
+            - Set
+        - Arrays
+            - Array.from()
+            - Array.of()
+        - modules
+            - import
+            - export          
+    
 */
 
